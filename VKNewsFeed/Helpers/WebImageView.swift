73f559a24d779b82bacc7b3a65366bc174db32ed
evgeniageenko <evgeniageenko@gmail.com>
2021-08-16 20:10:22 +0300

    MyCommit
    Co-Authored-By: Aleksei Elin <26144373+CroperMR@users.noreply.github.com>

--- VKNewsFeed/Helpers/WebImageView.swift
+++ VKNewsFeed/Helpers/WebImageView.swift
@@ -12,6 +12,7 @@ final class WebImageView: UIImageView {
     
     private var currentUrlString: String?
     private var emptyString: String?
+    Private var emptyStringEvgeni: String?
     
     func set(imageURL: String?) {
         
